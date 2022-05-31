684fe06e093f123d597d9a7100278155ae5713b9
Danrley Linhares Batista <danrleylinhares@gmail.com>
2022-05-31 13:23:26 -0300

    Final

--- api.py
+++ api.py
@@ -1,10 +1,39 @@
+from typing import List
 from fastapi import FastAPI, Path
 from typing import Optional
 from pydantic import BaseModel
+import databases
+import sqlalchemy
 
 app = FastAPI()
 
+DATABASE_URL = "postgresql://akybyuoiwbxfyv:1c670bb5d549dbfd04b07fdbec8605584dad996226d50b58ef09bdac7d24d423@ec2-3-234-131-8.compute-1.amazonaws.com:5432/d2nmme35a5k893"
+
+database = databases.Database(DATABASE_URL)
+
+metadata = sqlalchemy.MetaData()
+
+notes = sqlalchemy.Table(
+    "Cartas",
+    metadata,
+    sqlalchemy.Column("nome", sqlalchemy.String, primary_key=True),
+    sqlalchemy.Column("edicao", sqlalchemy.String),
+    sqlalchemy.Column("idioma", sqlalchemy.String),
+    sqlalchemy.Column("foil", sqlalchemy.Boolean),
+    sqlalchemy.Column("preco", sqlalchemy.Integer),
+    sqlalchemy.Column("quantidade", sqlalchemy.Float),
+)
+
+
+engine = sqlalchemy.create_engine(
+    DATABASE_URL
+)
+metadata.create_all(engine)
+
+
+
 cartas = []
+
 
 class Carta(BaseModel):
     nome: str
@@ -19,68 +48,56 @@ class CartaAtualizavel(BaseModel):
     preco: Optional[float] = None
     quantidade: Optional[int] = None
 
+
+
 @app.get("/")
 def hello_world_root():
     return {"Hello": "World"}
 
-@app.get('/Pesquisar-carta')
-def pequisar_carta(nome: Optional[str] = None):
 
-    search = list(filter(lambda x: x["nome"] == nome, cartas))
-
-    if search == []:
-        return {'Carta': 'Não existe'}
-
-    return {'Carta': search[0]}
+@app.on_event("startup")
+async def startup():
+    await database.connect()
 
 
-@app.get('/Listar-cartas')
-def listar():
-    return {'Cartas': cartas}
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
 
+@app.get("/Pesquisar-carta/", response_model=Carta)
+async def pequisar_carta(nome: Optional[str] = None):
+    
+    query =  notes.select().where(notes.c.nome == nome)
 
-@app.post('/Criar-carta')
-def criar_carta(nome_da_carta: str, carta: Carta):
-
-    search = list(filter(lambda x: x["nome"] == nome_da_carta, cartas))
-
-    if search != []:
-        return {'Error': 'Já existe.'}
-
-    carta = carta.dict()
-    carta['nome'] = nome_da_carta
-
-    cartas.append(carta)
-    return carta
-
-
-@app.put('/Atualizar-carta')
-def Atualizar_Carta(nome_da_carta: str, carta: CartaAtualizavel):
-
-    search = list(filter(lambda x: x["nome"] == nome_da_carta, cartas))
-
-    if search == []:
-        return {'Carta': 'Não existe'}
-
-    if carta.quantidade is not None:
-        search[0]['quantidade'] = carta.quantidade
-
-    if carta.preco is not None:
-        search[0]['preco'] = carta.preco
+    return await database.fetch_one(query)
     
 
-    return search
+@app.get("/Listar-Cartas/", response_model=List[Carta])
+async def listar_Cartas():
+    query = notes.select()
+    return await database.fetch_all(query)
 
 
-@app.delete('/deletar-carta')
-def Deletar_carta(nome_da_carta: str):
-    search = list(filter(lambda x: x["nome"] == nome_da_carta, cartas))
+@app.put('/Atualizar-carta', response_model=CartaAtualizavel)
+async def atualizar_Carta(nome_da_carta: str, carta: CartaAtualizavel):
 
-    if search == []:
-        return {'carta': 'Não existe'}
+    query = notes.update().where(notes.c.nome == nome_da_carta).values(preco=carta.preco, 
+    quantidade=carta.quantidade)
 
-    for i in range(len(cartas)):
-        if cartas[i]['nome'] == nome_da_carta:
-            del cartas[i]
-            break
-    return {'Message': 'carta deletada com sucesso!'}
+    await database.execute(query)
+    
+    return {**carta.dict(), "id": carta}
+
+
+@app.post("/Criar-Cartas/", response_model=Carta)
+async def criar_Cartas(carta: Carta):
+    query = notes.insert().values(nome=carta.nome, edicao=carta.edicao, idioma=carta.idioma, 
+    foil=carta.foil, preco=carta.preco, quantidade=carta.quantidade)
+    last_record_id = await database.execute(query)
+    return {**carta.dict(), "id": last_record_id}
+
+@app.delete("/deletar-carta/")
+async def deletar_carta(nome_da_carta: str):
+    query = notes.delete().where(notes.c.nome == nome_da_carta)
+    await database.execute(query)
+    return {"Messagem": "A carta deletada com sucesso!"}
